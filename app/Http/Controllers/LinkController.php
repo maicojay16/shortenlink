84209cd0e18af119dee76a2c2d7532a0536c8cb3
maicojay16 <jay@Jaaaaaay.local>
2019-01-03 13:38:01 +0800

    Add Insert function

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -3,19 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Link;
 
 class LinkController extends Controller
 {
 
     public function test(Request $request)
     {        
+        
+        $link = new Link;
+        $token = str_random(12);
+        $link->link = $request->input('link');
+        $link->token = $token;
+
+        try{
+            $link->save();
+        } catch(\Exception $e){
+            return response()->json(
+                [
+                    'status' => 'failed',
+                    'message' => 'could not create short link',
+                    'data' => []
+                ]
+            );
+        }
+
         return response()->json(
             [
                 'status' => 'success',
                 'message' => 'request is successful',
                 'data' => [
                     'link' => $request->input('link'),
-                    'short_link' => 'we do not do that here...',
+                    'short_link' => $token,
                 ]
             ]
         );
