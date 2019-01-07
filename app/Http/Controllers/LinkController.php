4cff37f5a31f4a519d9698e57600e2986a95effd
maicojay16 <jay@Jaaaaaay.local>
2019-01-07 12:27:09 +0800

    Edit Controller

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -10,29 +10,12 @@ class LinkController extends Controller
 {
 
     /*
-    * @param int $number
-    * @return mixed
-    */
-    private function Jay(int $number): int
-    {
-        return $number;
-    }
-
-
-    /*
     * Save Link data to the dababase 
     *
     * @param Request $request
     * @return Response
     */
     public function test(Request $request){        
-        // return $this->Jay(100);
-
-        $link = new Link;
-        $token = str_random(12);
-        $link->link = $request->input('link');
-        $link->token = $token;
-
         $validator = Validator::make($request->all(), [
             'link' => 'required|active_url',
         ]);
@@ -45,30 +28,35 @@ class LinkController extends Controller
                     'data' => []
                 ]
             );
-        }else{
-            try{
-                $link->save();
-            } catch(\Exception $e){
-                return response()->json(
-                    [
-                        'status' => 'failed',
-                        'message' => 'could not create short link',
-                        'data' => []
-                    ]
-                );
-            }
-    
+        }
+
+        $link = new Link;
+        $token = str_random(12);
+        $link->link = $request->input('link');
+        $link->token = $token;
+        try{
+            $link->save();
+        } catch(\Exception $e){
             return response()->json(
                 [
-                    'status' => 'success',
-                    'message' => 'request is successful',
-                    'data' => [
-                        'link' => $request->input('link'),
-                        'short_link' => $token,
-                    ]
+                    'status' => 'failed',
+                    'message' => 'could not create short link',
+                    'data' => []
                 ]
             );
         }
+    
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'request is successful',
+                'data' => [
+                    'link' => $request->input('link'),
+                    'short_link' => $token,
+                ]
+            ]
+        );
+        
     }
 
     /*
