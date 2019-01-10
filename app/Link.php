dedd1c37c1a01a507260eeeb0ea6017132f180d9
Jay <maicojay16@gmail.com>
2019-01-10 15:50:18 +0800

    Edit models add relations

--- app/Link.php
+++ app/Link.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Link extends Model
 {
 
+    public function linkAnalytic()
+    {
+        return $this->hasMany('App\LinkAnalytic', 'link_id', 'id');
+    }
 }
--- app/LinkAnalytic.php
+++ app/LinkAnalytic.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LinkAnalytic extends Model
 {
 
+    public function link()
+    {
+        return $this->hasOne('App\Link', 'id', 'link_id');
+    }
 }
